3480cab2d82bab27d88386c092b37574e51845ba
Muhammad Junaid <70976999+Johnny463@users.noreply.github.com>
2025-11-22 03:36:57 +0500

    Add files via upload

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,18 @@
+export const API_URL =
+  "https://video-sharing-backend-d4bwckg2excdbggc.canadacentral-01.azurewebsites.net/";
+
+export async function fetchVideos() {
+  const res = await fetch(`${API_URL}/api/videos`);
+  if (!res.ok) throw new Error("Failed to fetch videos");
+  return res.json();
+}
+
+export async function uploadVideo(data: any) {
+  const res = await fetch(`${API_URL}/api/upload`, {
+    method: "POST",
+    body: data,
+  });
+
+  if (!res.ok) throw new Error("Upload failed");
+  return res.json();
+}
